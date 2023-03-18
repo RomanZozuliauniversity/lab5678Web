f88517efa95298686bd61ff2d81daa4ec78fe086
Roman Zozulia <48176098+romanzeze@users.noreply.github.com>
2023-03-18 19:49:57 +0000

    dtn

--- lab5Web.js
+++ lab5Web.js
@@ -1,3 +1,4 @@
+//1
 function compareArrays() {
 
     var array1 = document.getElementById("array1").value;
@@ -17,6 +18,71 @@ function compareArrays() {
             result.push(array2[i]);
         }
     }
-
-    document.getElementById("result").innerHTML = "Result: " + result;
+    document.getElementById("resultfirst").innerHTML = "Result: " + result;
 }
+
+//2
+function printDescending() {
+    var n = parseInt(document.getElementById("numberInput").value);
+    var output = "";
+    for (var i = n; i >= 0; i--) {
+        output += i + "<br>";
+    }
+    document.getElementById("output").innerHTML = output;
+}
+//3
+function generateArray() {
+    const numberInput = document.getElementById('number-input');
+    const number = Number(numberInput.value);
+    
+    const array = [];
+    for (let i = 1; i <= number; i++) {
+      array.push('x'.repeat(i));
+    }
+    
+    const resultElement = document.getElementById('resulttask3');
+    resultElement.textContent = array.join(', ');
+  }
+//4
+function isPrime(n) {
+    if (n < 2) return false;
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i === 0) return false;
+    }
+    return true;
+  }
+  
+  function checkPrimeNumbers() {
+    const input = document.getElementById("numbers").value;
+    const numbers = input.split(",");
+    const resultTable = document.getElementById("result4");
+    
+    // clear previous results
+    resultTable.innerHTML = "";
+    
+    // add header row
+    const headerRow = document.createElement("tr");
+    const numberHeader = document.createElement("th");
+    numberHeader.textContent = "Число";
+    const primeHeader = document.createElement("th");
+    primeHeader.textContent = "Просте";
+    headerRow.appendChild(numberHeader);
+    headerRow.appendChild(primeHeader);
+    resultTable.appendChild(headerRow);
+    
+    // add rows for each number
+    for (let i = 0; i < numbers.length; i++) {
+      const row = document.createElement("tr");
+      const numberCell = document.createElement("td");
+      numberCell.textContent = numbers[i];
+      row.appendChild(numberCell);
+      const primeCell = document.createElement("td");
+      if (isPrime(numbers[i])) {
+        primeCell.textContent = "Так";
+      } else {
+        primeCell.textContent = "Ні";
+      }
+      row.appendChild(primeCell);
+      resultTable.appendChild(row);
+    }
+  }
