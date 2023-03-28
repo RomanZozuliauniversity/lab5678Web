b966727f23aa90b58bb940535b7906d6a211663f
Roman Zozulia <48176098+romanzeze@users.noreply.github.com>
2023-03-28 19:37:49 +0000

    fdad

--- js/lab8.js
+++ js/lab8.js
@@ -9,3 +9,11 @@ input.addEventListener('mousedown', event => {
 input.addEventListener('mouseup', event => {
     input.classList.remove('clicked');
 });
+
+
+const elementSelect = document.getElementById('element');
+const resultInput = document.getElementById('result');
+elementSelect.addEventListener('change', function() {
+  const selectedOption = elementSelect.options[elementSelect.selectedIndex];
+  resultInput.value = 'Selected item: ' + selectedOption.text;
+});
